34e95ac11b44cd1fb941bed9fecb5cc83f849295
julien henry <julienhenry967@gmail.com>
2024-05-31 19:21:06 +0200

    init

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+const Card = ({ iconUrl, title, description }) => {
+  return (
+    <div className="feature-item">
+      <img src={iconUrl} alt="" className="feature-icon" />
+      <h3 className="feature-item-title">{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Card;
